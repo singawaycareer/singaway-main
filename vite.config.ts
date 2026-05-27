9cc5d620fea9a23f716d386fef81938e98493c94
Kunal Madoliya <madoliyakunal@gmail.com>
2026-05-27 22:55:49 +0530

    feat: add nitro dependency and configure build environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,24 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
+
     devtools(),
+
+    nitro({
+      rollupConfig: {
+        external: [/^@sentry\//],
+      },
+    }),
+
     tailwindcss(),
+
     tanstackStart(),
+
     viteReact(),
   ],
 })
